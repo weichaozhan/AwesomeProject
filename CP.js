98fc1c8d5780fbe5407992ec11f302edb12c3d14
weichaozhan <921391554@qq.com>
2019-02-14 10:33:54 +0800

    hello

--- CP.js
+++ CP.js
@@ -2,12 +2,41 @@ import React, {Component} from 'react';
 import {StyleSheet, View, Text} from 'react-native';
 
 export default class CP extends Component {
+	constructor(props) {
+		super(props);
+		
+		this.state = {
+			show: false,
+		};
+		this.timer = null;
+	}
+
+	componentDidMount() {
+		this.timer = setInterval(() => {
+			this.setState({
+				show: !this.state.show,
+			});
+		}, 100);
+	}
+
+	componentWillUnmount() {
+		clearInterval(this.timer);
+	}
+
 	render() {
 		return (
 			<View>
 				<Text>
 					{this.props.name}
 				</Text>
+				{this.state.show &&
+					<Text>
+						闪烁
+					</Text>
+				}
+				<Text>
+						不闪烁
+					</Text>
 			</View>
 		);
 	}
